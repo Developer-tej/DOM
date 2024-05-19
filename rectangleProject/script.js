b6bcc1a9033ec2d6f111135a18d88009946d75fe
Developer Tej <shivatej.developer@gmail.com>
2024-05-20 02:07:00 +0530

    Add the DOM for changing shape in script file

--- rectangleProject/script.js
+++ rectangleProject/script.js
@@ -1,1 +1,16 @@
-alert('hello');
+let shape = document.querySelector('.shape');
+let btn = document.querySelector('button');
+let flag = 0;
+
+btn.addEventListener('click',()=>{
+    if (flag == 0){ 
+        shape.classList = 'circle';
+        flag++;
+    }
+    else {
+        shape.classList = 'shape';
+        flag--;
+    }
+})
+
+
